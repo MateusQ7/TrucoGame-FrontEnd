7389d56b4ef313270dac301534c15f8dbc7a2621
mateusquixada <mateusquixada@gmail.com>
2023-12-13 16:15:52 -0300

    feat: Adiciona modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FooterBarComponent } from './shared/footer-bar/footer-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
