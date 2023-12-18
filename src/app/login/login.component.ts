c5ca9741e1137fa914832226b629ccfac6132867
mateusquixada <mateusquixada@gmail.com>
2023-12-18 17:23:53 -0300

    fix: Inicia form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  form!: FormGroup
+
+  constructor(private FormBuilder: FormBuilder){
+
+  }
+  ngOnInit(): void {
+    this.form = this.FormBuilder.group({
+      user: [{
+        value: '',
+        disabled: false
+      },[
+        Validators.required,
+
+      ]],
+      password: [{
+        value:'',
+        disabled: false
+      },[
+        Validators.required,
+      ]]
+
+    })
+  }
 
 }
