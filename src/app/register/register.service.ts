33ae439f7558cce376b7d350a8a582d7dffd2b0f
mateusquixada <mateusquixada@gmail.com>
2024-01-15 16:55:12 -0300

    feat: Adiciona link da api e metodos http

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormatedForm } from '../models/formatedForm';
+import { Observable } from 'rxjs';
+import { BackReponse } from './backResponse.interface';
+import { ConfigService } from '../services/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private config: ConfigService
+    ) { }
+
+  submit(formData: FormatedForm): Observable<BackReponse>{
+    return this.http.post<BackReponse>(`${this.config.apiUrl}/user`, formData);
+  }
 }
