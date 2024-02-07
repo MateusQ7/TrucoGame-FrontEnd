9aa92ccb6d73bcaa6aba546ac915fa0abfe0e249
mateusquixada <mateusquixada@gmail.com>
2024-02-07 13:43:03 -0300

    fix: refatora async login

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ConfigService } from '../services/config.service';
+import { FormatedForm } from '../models/formatedForm';
+import { BackReponse } from '../register/backResponse.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private config: ConfigService
+    ) { }
+
+    submit(formData: FormatedForm): Observable<BackReponse>{
+      return this.http.post<BackReponse>(`${this.config.apiUrl}/login`, formData);
+    }
 }
